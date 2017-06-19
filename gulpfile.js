db11b5f3f45ccfe06163ea0914f20de629527f83
ferdiemmen <fjemmen@gmail.com>
2017-06-19 11:19:55 +0200

    Shamelessly copied demo from plyr
    And added gulp task "demo"

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let gulp            = require('gulp');
+let sass            = require('gulp-sass');
+let browserSync     = require('browser-sync');
+
+
+gulp.task('demo', ['demo:build'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './demo'
+    }
+  });
+
+  gulp.watch('./src/scss/**/*.scss', ['demo:build:css']);
+  gulp.watch('./src/js/**/*.js', ['demo:build:js']);
+});
+
+
+gulp.task('demo:build',
+  [
+    'demo:build:css',
+    'demo:build:js',
+  ], (done) => {
+
+  done();
+});
+
+
+gulp.task('demo:build:css', () => {
+  return gulp.src('./src/scss/plyr-ads.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./demo/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('demo:build:js', () => {
+  return gulp.src('./src/js/plyr-ads.js')
+    .pipe(gulp.dest('./demo/js'))
+    .pipe(browserSync.reload({stream: true}));
+})
