d05396f8e47607c4f9f13aebfd6396e16d4132df
ferdiemmen <fjemmen@gmail.com>
2017-06-22 13:16:52 +0200

    add touchstart

--- src/js/plyr-ads.js
+++ src/js/plyr-ads.js
@@ -113,7 +113,7 @@
     this.adDisplayContainer = new window.google.ima.AdDisplayContainer(
       this.plyr.getContainer());
     this.adDisplayContainer.I.setAttribute('class', 'plyr-ads');
-    this.adDisplayContainer.I.addEventListener('click', function() {
+    this.adDisplayContainer.I.addEventListener(_getStartEvent(), function() {
       this.playAds();
     }.bind(this), false);
   }
@@ -133,12 +133,23 @@
       if ((skipTimer + 1) === 0) {
         this.skipAdButton.className += ' done';
         this.skipAdButton.innerHTML = this.options.skipButton.text;
-        this.skipAdButton.addEventListener('click', function() {
+        this.skipAdButton.addEventListener(_getStartEvent(), function() {
           this.playVideo();
         }.bind(this), false);
         window.clearInterval(skipButtonTimer);
       }
     }.bind(this), 1000);
+  }
+
+  function _getStartEvent() {
+    // Set the correct event based on userAgent.
+    var startEvent = 'click';
+    if (navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/Android/i)) {
+      startEvent = 'touchstart';
+    }
+    return startEvent;
   }
 
   // Prepend child
