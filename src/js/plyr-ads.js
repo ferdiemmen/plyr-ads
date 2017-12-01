ce826127680e1bd366764131484765a55d4c80c7
ferdiemmen <fjemmen@gmail.com>
2017-12-01 16:29:43 +0100

    Get a hold of the adDisplayContainer

--- src/js/plyr-ads.js
+++ src/js/plyr-ads.js
@@ -6,11 +6,35 @@ class PlyrAds {
   constructor(target, options) {
     this.config = this._mergedConfig(defaults, options);
     this.plyr = target;
+    this.adDisplayContainer;
 
-    // Set listeners on plyr media events
+    if (!this.config.adTagUrl) {
+      throw Error('No adTagUrl provided.');
+    }
+
+    this._createAdDisplayContainer();
+
+    // Request video ads.
+    // this.adsRequest = new google.ima.AdsRequest();
+    // adsRequest.adTagUrl = config.adTagUrl;
+
+    // Set listeners on plyr media events.
     this._setListeners();
   }
 
+
+  _createAdDisplayContainer() {
+    // We assume the adContainer is the video wrapper of the plyr element that will house
+    // the ads.
+    const { wrapper, original } = this.plyr.elements;
+
+    this.adDisplayContainer = new google.ima.AdDisplayContainer(
+        wrapper, original);
+
+    console.log(this.adDisplayContainer);
+  }
+
+  // Merge defaults and options.
   _mergedConfig(defaults, options) {
     return {...defaults, ...options};
   }
@@ -20,7 +44,6 @@ class PlyrAds {
     // Progress
     this.plyr.on('progress', (event) => {
       const { currentTime } = event.detail.plyr;
-      console.log(currentTime);
     });
   }
 }
