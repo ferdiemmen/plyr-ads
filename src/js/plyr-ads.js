7a4afcb06678915853baecb9f9540c883b81b053
Ferdi Emmen <fjemmen@gmail.com>
2017-12-02 09:08:16 +0100

    Add setup of ad display container

--- src/js/plyr-ads.js
+++ src/js/plyr-ads.js
@@ -7,31 +7,42 @@ class PlyrAds {
     this.config = this._mergedConfig(defaults, options);
     this.plyr = target;
     this.adDisplayContainer;
+    this.adDisplayElement;
 
-    if (!this.config.adTagUrl) {
-      throw Error('No adTagUrl provided.');
-    }
+    // Check if a adTagUrl us provided.
+    if (!this.config.adTagUrl) { throw Error('No adTagUrl provided.'); }
 
-    this._createAdDisplayContainer();
+    // Check if the Google IMA3 SDK is loaded.
+    if (!window.google) { throw new Error('The Google IMA3 SDK is not loaded.'); }
+
+    // Setup the ad display container.
+    this._setupAdDisplayContainer();
 
     // Request video ads.
-    // this.adsRequest = new google.ima.AdsRequest();
-    // adsRequest.adTagUrl = config.adTagUrl;
+    this.adsRequest = new google.ima.AdsRequest();
+    this.adsRequest.adTagUrl = this.config.adTagUrl;
 
     // Set listeners on plyr media events.
     this._setListeners();
   }
 
 
-  _createAdDisplayContainer() {
+  _setupAdDisplayContainer() {
+    const { wrapper, original } = this.plyr.elements;
+    
     // We assume the adContainer is the video wrapper of the plyr element that will house
     // the ads.
-    const { wrapper, original } = this.plyr.elements;
-
     this.adDisplayContainer = new google.ima.AdDisplayContainer(
         wrapper, original);
 
-    console.log(this.adDisplayContainer);
+    this.adDisplayElement = wrapper.firstChild;
+
+    // The AdDisplayContainer call from google ima sets the style attribute
+    // by default. We remove the inline style and set it through the stylesheet.
+    this.adDisplayElement.removeAttribute('style');
+    
+    // Set class name on the adDisplayContainer element
+    this.adDisplayElement.setAttribute('class', 'plyr-ads');
   }
 
   // Merge defaults and options.
